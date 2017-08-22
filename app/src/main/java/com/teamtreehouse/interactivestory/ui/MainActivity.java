2f8f6e7b623ecbfe00160a20bc92dd6120a12569
Joseph Wagner <joseph.j.wagner360@gmail.com>
2017-08-22 05:50:24 -0700

    Commit 8222017

--- app/src/main/java/com/teamtreehouse/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/ui/MainActivity.java
@@ -1,12 +1,14 @@
-package com.teamtreehouse.interactivestory;
+package com.teamtreehouse.interactivestory.ui;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.teamtreehouse.interactivestory.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText nameField;
@@ -31,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void startStory(String name) {
         Intent intent = new Intent(this, StoryActivity.class);
-        intent.putExtra("name", name);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
         startActivity(intent);
     }
 }
